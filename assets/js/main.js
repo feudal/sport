cf3b41225f5beb77f77571539a79104ddae539d0
feudal <calmis87@gmail.com>
2020-07-09 09:10:09 +0300

    first init2

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.big-slider').slick({
+    dots: true,
+    vertical: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    verticalSwiping: true,
+  });
+
+});
